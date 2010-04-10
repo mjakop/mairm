19a71f61de90bd5b7052dab9e60eac83e46b4a32
mjakop <mjakop@ea20db8a-6231-27a7-6703-bcca998dd19d>
2010-04-10 13:32:23 +0000

    Mouse control is now working.

--- src/Demo.java
+++ src/Demo.java
@@ -1,98 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by                                                 *
+ *   	Matej Jakop <matej@jakop.si>                                       *
+ *      Gregor Kališnik <gregor@unimatrix-one.org>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License version 3        *
+ *   as published by the Free Software Foundation.                         *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ ***************************************************************************/
 
+import java.awt.MouseInfo;
 import java.awt.Robot;
-import java.io.IOException;
-import java.util.Vector;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import javax.bluetooth.BluetoothStateException;
-import javax.bluetooth.DataElement;
-import javax.bluetooth.DeviceClass;
-import javax.bluetooth.DiscoveryAgent;
-import javax.bluetooth.DiscoveryListener;
-import javax.bluetooth.LocalDevice;
 import javax.bluetooth.RemoteDevice;
-import javax.bluetooth.ServiceRecord;
 import javax.bluetooth.UUID;
 import javax.microedition.io.Connector;
-import javax.obex.ClientSession;
-import javax.obex.HeaderSet;
-
-import com.intel.bluetooth.BluetoothStack;
-import com.intel.bluetooth.obex.BlueCoveOBEX;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
 
 import lib.*;
-
-class Poslusalec implements DiscoveryListener {
-
-			Vector<RemoteDevice> naprave=new Vector<RemoteDevice>();
-			Vector<ServiceRecord> service=new Vector<ServiceRecord>();
-	
-            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-            	naprave.add(btDevice);
-            	System.out.println("Nasel napravo.");
-            }
-
-            public void inquiryCompleted(int discType) {
-            }
-
-            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-                for (int i = 0; i < servRecord.length; i++) {
-                	service.add(servRecord[i]);
-
-                    String url = servRecord[i].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
-                    if (url == null) {
-                        continue;
-                    }
-                    DataElement serviceName = servRecord[i].getAttributeValue(0x0100);
-                    if (serviceName != null) {
-                        System.out.println("service " + serviceName.getValue() + " found " + url);
-                    } else {
-                        System.out.println("service found " + url);
-                    }
-
-                }
-            }
-
-            public void serviceSearchCompleted(int transID, int respCode) {
-            }
-
-}
 
 public class Demo {
 	
 	public static void main(String[] args) throws Exception{
-Poslusalec po=new Poslusalec();
-LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, po);
-Thread.sleep(7000);
-UUID serviceUUID = new UUID(0x0003); //obex file transfer
-UUID[] searchUuidSet = new UUID[] { serviceUUID };
-int[] attrIDs =  new int[] {
-        0x0100 // Service name
-};
-
-
-for (RemoteDevice naprava: po.naprave) {
-	LocalDevice.getLocalDevice().getDiscoveryAgent().searchServices(null,searchUuidSet,naprava,po);
-}
-Thread.sleep(3000);
-System.out.println("Povezal se bom.");
-for (ServiceRecord serv: po.service) {
-try {
-	ClientSession clientSession = (ClientSession) Connector.open("btspp://001C9A246CDD:5;authenticate=false;encrypt=false;master=false");
-} catch (IOException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-}
 		System.out.println(MAIRObject.version());
 		MAIR m=new MAIR();
 		//with input we specify from where lib should read data
-		MAIRInput input=new MAIRInputComPort();
+		int i=100+(int)(Math.random()*100);
+		MAIRInput input=new MAIRInputBluetooth("MAIR"+i);
 		m.setInput(input);
+		m.loadGesturesFromFile("demo.txt");
 		m.start();
-		//or
-		m.startLearnMode();
-		Thread.sleep(1000);
-		m.stop();
-		System.out.println("Finished");
 	}
 }
