71d59f37a66cf1a2459bcc46d1135a21ae5805a1
gregor.kalisnik <gregor.kalisnik@ea20db8a-6231-27a7-6703-bcca998dd19d>
2010-05-07 11:36:24 +0000

    Added a buffer for clicks. Added partial support for Nokia 5800 XM.

--- python/mairm-client/sensors.py
+++ python/mairm-client/sensors.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sensor
 
 class AbstractSensor:
@@ -33,8 +34,34 @@ class OldSensor(AbstractSensor):
   def cleanup(self):
     self.sensor.disconnect()
 
+class S605thSensor(AbstractSensor):
+  def __init__(self):
+    self.sensor = sensor.AccelerometerXYZAxisData()
+    self.sensor.set_measure_range(0)
+    self.cb = None
+  
+    self.sensor.set_callback(data_callback = self.coord_cb)
+    self.sensor.start_listening()
+    self.count = 0
+  
+  def coord_cb(self):
+    if self.cb is None:
+      return
+    
+    # Need to switch x and y coordinates for 5800
+    self.cb(self.sensor.y * 2, self.sensor.x * 2, self.sensor.z * 2)
+  
+  def set_callback(self, cb):
+    self.cb = cb
+  
+  def cleanup(self):
+    self.sensor.stop_listening()
+
 def init_sensor():
   try:
     return OldSensor()
   except:
-    raise Exception('No supported accelometer sensors API.')
+    try:
+      return S605thSensor()
+    except:
+      raise Exception('No supported accelometer sensors API.')
