820449a9c6158fc09652ea804c4d538e30540159
gregor.kalisnik <gregor.kalisnik@ea20db8a-6231-27a7-6703-bcca998dd19d>
2010-05-12 18:25:49 +0000

    Dodal parsanje tipkovnice.

---
+++ trunk/python/mairm-client/parsers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def parse_string(text):
+  output = u''
+  for char in text:
+    modifiers = u''
+    if char == u'@':
+      char = u'AT'
+    elif char == u'"':
+      char = u'DOUBLE QUOTE'
+    elif char == u"'":
+      char = u'QUOTE'
+    elif char.isupper():
+      char = char.lower()
+      modifiers += u'SHIFT'
+    
+    output += u'{"keyboard":{"keys":"%s", "modifiers":"%s"}}\n' % (char, modifiers)
+  
+  return output
